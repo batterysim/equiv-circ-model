c3cc399c2c44400c6551d5e5cfa88e60598eb7ce
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2020-02-03 16:38:05 -0500

    Remove old files and folders

--- ecm/printer/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-
-from .print_coeffs import print_coeffs
-from .print_parameters import print_parameters
-from .print_rctau import print_rctau
-from .print_soc_ocv import print_soc_ocv
